f885b09b417421827630f2e9d699e281dfc31cff
Nikola-Pet <82818137+Nikola-Pet@users.noreply.github.com>
2022-02-27 23:11:44 +0100

    Initial commit

---
+++ Weather/Models/WeatherModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weather.Models
+{
+    public class WeatherModel
+    {
+        public string Title { get; set; } = " ";
+        public string Temperature { get; set; } = " ";
+        public string Wind { get; set; } = " ";
+        public string Humidity { get; set; } = " ";
+        public string Visibility { get; set; } = " ";
+        public string Sunrise { get; set; } = " ";
+        public string Sunset { get; set; } = " ";
+    }
+}
